b89c11f9988cab28dff62cb9894191e1d7c5f80c
jaeseok <wotjr5124@gmail.com>
2024-07-15 17:30:21 +0900

    34 ing

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -6,6 +6,8 @@ function add (num1, num2) {
     return result;
 }
 
+addBtn.addEventListener('click' , add)
+
 currentResult = add(1, 2);
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`
