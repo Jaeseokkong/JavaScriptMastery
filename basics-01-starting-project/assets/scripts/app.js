d3b7ab6e9fa05f4908d10b0f0e2cd52100ae70a5
jaeseok <wotjr5124@gmail.com>
2024-07-16 17:30:42 +0900

    38 ing

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,16 +1,12 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add (num1, num2) {
-    const result = num1 + num2;
-    return result;
+function add () {
+    const enteredNumber = parseInt(userInput.value) //+userInput.value; => parseInt의 축약형
+    const calcDescription = `${currentResult} + ${enteredNumber}`
+    currentResult = currentResult + enteredNumber;
+    outputResult(currentResult, calcDescription);
+    userInput.value = ''
 }
 
 addBtn.addEventListener('click' , add)
-
-currentResult = add(1, 2);
-
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`
-
-outputResult(currentResult, calculationDescription);
-  
