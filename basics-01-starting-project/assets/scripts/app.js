00637886b9502b3395ecfd08c5f261719f719f79
jaeseok <wotjr5124@gmail.com>
2024-07-17 17:28:16 +0900

    48

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,12 +1,68 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
+
+
+// input 값 가져오기
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
+// 계산 로그 작성
+function createAndWriteOutput(operator, resultBeforCalc, calcNumber) {
+    const calcDescription = `${resultBeforCalc} ${operator} ${calcNumber}`
+    outputResult(currentResult, calcDescription); // from vender file
+}
+
+function writeToLog(
+    operationIdentifier,
+    prevResult,
+    operationNumber,
+    newResult
+) {
+    const logEntry = {
+        operation: operationIdentifier,
+        preResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries)
+}
 
 function add () {
-    const enteredNumber = parseInt(userInput.value) //+userInput.value; => parseInt의 축약형
-    const calcDescription = `${currentResult} + ${enteredNumber}`
-    currentResult = currentResult + enteredNumber;
-    outputResult(currentResult, calcDescription);
-    userInput.value = ''
+    const enteredNumber = getUserNumberInput() //+userInput.value; => parseInt의 축약형
+    const initialResult = currentResult;
+    currentResult += enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber)
+    writeToLog('ADD', initialResult, enteredNumber, currentResult)
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput() 
+    const initialResult = currentResult;
+    currentResult -= enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber)
+    writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult)
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput() 
+    const initialResult = currentResult;
+    currentResult *= enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber)
+    writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult)
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput() 
+    const initialResult = currentResult;
+    currentResult /= enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber)
+    writeToLog('DIVIDE', initialResult, enteredNumber, currentResult)
 }
 
 addBtn.addEventListener('click' , add)
+subtractBtn.addEventListener('click' , subtract)
+multiplyBtn.addEventListener('click' , multiply)
+divideBtn.addEventListener('click' , divide)
