a2c867e94ddc92f8e9a7ebc064aef4f42fe73d33
jaeseok <wotjr5124@gmail.com>
2024-09-06 17:26:55 +0900

    break

--- attack-game/assets/scripts/app.js
+++ attack-game/assets/scripts/app.js
@@ -15,6 +15,7 @@ const enteredValue = prompt('Maximum life for you and the monster.', '100');
 
 let chosenMaxLife = parseInt(enteredValue);
 let battleLog = [];
+let lastLoggedEntry;
 
 if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
   chosenMaxLife = 100;
@@ -223,17 +224,21 @@ function printLogHandler() {
   // for (let i = 0; i < battleLog.length; i++) {
   //   console.log(battleLog[i])
   // }
-  let j = 0;
-  while (j < 3) {
-    console.log('----------')
-    j++;
-  }
+  // let j = 0;
+  // while (j < 3) {
+  //   console.log('----------')
+  //   j++;
+  // }
 
   let i = 0;
   for (const logEntry of battleLog) {
-    console.log(`#${i}`)
-    for (const key in logEntry) {
-      console.log(`${key} => ${logEntry[key]}`)
+    if ((!lastLoggedEntry && lastLoggedEntry !== 0) || lastLoggedEntry < i) {
+      console.log(`#${i}`)
+      for (const key in logEntry) {
+        console.log(`${key} => ${logEntry[key]}`)
+      }
+      lastLoggedEntry = i;
+      break;
     }
     i++
   }
