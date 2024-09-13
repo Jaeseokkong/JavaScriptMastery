ef14943cb32ccd2f0d783446d17428a2d6cb6483
jaeseok <wotjr5124@gmail.com>
2024-09-13 11:52:42 +0900

    var let

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -1,17 +1,26 @@
-var name = 'Max';
+// var name = 'Max';
 
-if (name === 'Max') {
-    // var hobbies = ['Sports', 'Cooking'];
-    let hobbies = ['Sports', 'Cooking'];
-    console.log(hobbies);
-}
+// if (name === 'Max') {
+//     // var hobbies = ['Sports', 'Cooking'];
+//     let hobbies = ['Sports', 'Cooking'];
+//     console.log(hobbies);
+// }
 
-function greet() {
-    let age = 30;
-    let name = 'Manuel'
-    console.log(name, age, hobbies)
-}
+// function greet() {
+//     let age = 30;
+//     let name = 'Manuel'
+//     console.log(name, age, hobbies)
+// }
 
-console.log(name, hobbies)
+// console.log(name, hobbies)
 
-greet();
+// greet();
+// var userName = 'Max';
+// var userName = 'Manuel' //재선언 가능
+
+let userName = 'Max';
+//let userName = 'zzz' // 재선언 불가능
+
+console.log(userName);
+
+//let userName = 'Max'; //error
