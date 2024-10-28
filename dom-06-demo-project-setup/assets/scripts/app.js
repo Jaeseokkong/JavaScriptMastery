5ed86bf7c82435cbff67d4e756151fef21428d0f
jaeseok <wotjr5124@gmail.com>
2024-10-28 11:54:24 +0900

    delete event

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -21,7 +21,22 @@ const updateUI = () => {
     }
 };
 
-const renderNewMovieElement = (title, imageUrl, rating) => {
+const deleteMovieHandler = (movieId) => {
+    let movieIndex = 0;
+    for (const movie of movies) {
+        if (movie.id === movieId) {
+            break;
+        }
+        movieIndex++;
+    }
+    movies.splice(movieIndex, 1);
+    const listRoot = document.getElementById('movie-list');
+    console.log(listRoot.children)
+    listRoot.children[movieIndex].remove();
+
+}
+
+const renderNewMovieElement = (id, title, imageUrl, rating) => {
     const newMovieElement = document.createElement('li');
     newMovieElement.className = 'movie-element';
     newMovieElement.innerHTML = `
@@ -33,7 +48,7 @@ const renderNewMovieElement = (title, imageUrl, rating) => {
             <p>${rating}</p>
         </div>
     `;
-
+    newMovieElement.addEventListener('click', deleteMovieHandler.bind(null, id));
     const listRoot = document.getElementById('movie-list');
     listRoot.append(newMovieElement);
 }
@@ -75,6 +90,7 @@ const addMovieHandler = () => {
     }
 
     const newMovie = {
+        id: Math.random().toString() ,
         title: titleValue,
         image: imageUrlValue,
         rating: ratingValue
@@ -84,7 +100,7 @@ const addMovieHandler = () => {
     console.log(movies);
     toggleMovieModal();
     clearMovieInput();
-    renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+    renderNewMovieElement(newMovie.id, newMovie.title, newMovie.image, newMovie.rating);
     updateUI();
 }
 
@@ -95,4 +111,4 @@ const backdropClickHandler = () => {
 startAddMovieButton.addEventListener('click', toggleMovieModal);
 backdrop.addEventListener('click', backdropClickHandler);
 cancelAddMovieButton.addEventListener('click', cancelAddMovieHandler);
-confirmAddMovieButton.addEventListener('click', addMovieHandler);
+confirmAddMovieButton.addEventListener('click', addMovieHandler);;
